02070eaed68f7f90456c28a0042fcd4f52a5b2c0
manan427 <modimanan00@gmail.com>
2023-03-22 17:36:37 +0530

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './pages/Home';
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
