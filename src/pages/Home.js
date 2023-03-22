dc73be250a90948bdb7019926727e20e2f0389ab
manan427 <modimanan00@gmail.com>
2023-03-22 17:21:18 +0530

    home page satrted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import BannerImage from '../assests/pizza.jpeg'
+import "../styles/Home.css"
 function Home() {
   return (
-    <div>
-      Hello World
+    <div className='home'>
+      <div className='headerContainer' style={{backgroundImage:`url(${BannerImage})`}}>
+        <h1>Modi's Pizzeria</h1>
+        <p>Bachpan</p>
+        <Link to ="/menu">
+        <button>ORDER NOW</button>
+        </Link>
+      </div>
     </div>
   )
 }
